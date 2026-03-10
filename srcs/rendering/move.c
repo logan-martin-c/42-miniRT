356232366586fa21ed18a4d1e2715037d94e448a
Logan <contact@loganmartin.fr>
2026-03-10 15:18:02 +0100

    test

--- srcs/rendering/move.c
+++ srcs/rendering/move.c
@@ -26,20 +26,12 @@ void	rotate_cam(t_cam_data *cam, t_mlx_data *mlx, bool *rotating,
 	mlx_mouse_get_pos(mlx->mlx, mlx->win, &mouse_p.x, &mouse_p.y);
 	if (init_pos(&mouse_p, &last_mouse_p))
 		return ;
-	if (mouse_p.x > WIN_WIDTH * 0.5 - 1 || mouse_p.y > WIN_HEIGHT * 0.5 - 1
-		|| mouse_p.x < -WIN_WIDTH * 0.5 + 1 || mouse_p.y < -WIN_HEIGHT * 0.5 + 1)
-	{
-		last_mouse_p.x -= mouse_p.x;
-		last_mouse_p.y -= mouse_p.y;
-		mouse_p.x = 0;
-		mouse_p.y = 0;
-		mlx_mouse_move(mlx->mlx, mlx->win, WIN_WIDTH * 0.5, WIN_HEIGHT * 0.5);
-	}
+	center_mouse(&mouse_p, &last_mouse_p, mlx);
 	*rotating = true;
 	if (!obj)
 	{
 		cam->forward = vector_rot((t_vect3){0, 0, 1},
-				last_mouse_p.y / 1e3, last_mouse_p.x / 1e3, 0);
+				(mouse_p.y - last_mouse_p.y) / 1e3, (mouse_p.y - last_mouse_p.x) / 1e3, 0);
 		cam->right = vector_norm(vector_cross((t_vect3){0, 1, 0},
 					cam->forward));
 		cam->up = vector_norm(vector_cross(cam->forward, cam->right));
